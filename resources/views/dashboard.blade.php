85a7e222644e3120c76988a0300ed12f44aa17f5
Dhivyaaug <dhivyaeswaran2004@gmail.com>
2023-10-03 08:34:50 +0530

    Dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,47 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
+    
+        
+            <div class="bg-white mt-8 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+          {{ __("Exam name") }}
+                </div>
+            </div>
+            <div class="bg-white mt-8 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("Subject Name") }}
+                </div>
+            </div>
+            <div class="bg-white mt-8 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("Time session") }}
+                </div>
+            </div>
+            <div class="bg-white mt-8 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("Block Name") }}
+                </div>
+            </div>
+            <div class="bg-white mt-8 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("Floor No") }}
+                </div>
+            </div>
+            <div class="bg-white mt-8 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("Room No") }}
+                </div>
+            </div>
+            <div class="bg-white mt-8 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("Seat No") }}
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
